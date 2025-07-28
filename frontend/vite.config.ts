daa76515ffba38391f0c45106e7356a58a528ec5
Dimiqhz <dimiqhz@gmail.com>
2025-07-28 08:50:43 +0300

    Fix frontend answer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 3000,
-    strictPort: true
+    strictPort: true,
+    fs: {
+      strict: false,
+    }
+  },
+  preview: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    outDir: 'dist',
   }
-});
+})
